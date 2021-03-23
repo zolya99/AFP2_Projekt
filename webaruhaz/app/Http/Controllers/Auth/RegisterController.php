5e2994c9d7da2b2d455e5aa46f118bd6ca77f232
Nagy Zoltan <zolya1999@gmail.com>
2021-03-23 15:55:15 +0100

    Regisztrációhoz szükséges adatok Backend oldalon kiegészítve

--- webaruhaz/app/Http/Controllers/Auth/RegisterController.php
+++ webaruhaz/app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,11 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'birth' => ['required', 'date'],
+            'sex' => ['required'],
+            'address' => ['required', 'string', 'max:255'],
+            'phone_number' => ['required', 'int'],
+
         ]);
     }
 
@@ -65,9 +70,14 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'id' => $data['id'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'birth' => $data['birth'],
+            'sex' => $data['sex'],
+            'address' => $data['address'],
+            'phone_number' => $data['phone_number'],
+            'name' => $data['name'],
         ]);
     }
 }
