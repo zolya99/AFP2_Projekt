fec094084779b1c4a17ebf615d82e8c579af8c66
Nyeste Reka <rekabeka37@gmail.com>
2021-04-16 13:42:51 +0200

    searchType function hozzáadása a productControllerbe

--- webaruhaz/app/Drink.php
+++ webaruhaz/app/Drink.php
@@ -9,6 +9,23 @@ class Drink extends Model
     public static function search($input){
         return Drink::query()->where('name', 'like', '%' . $input . '%');
     }
+    public static function searchType($input)
+    {
+        $d_t = Drink_type::query()->where((Drink::where('type', 'like', '%' . $input . '%')->id), '==', 'type')->get();
 
+        try {
+            if ($d_t->count() > 0)
+                $ans = Drink::query()->where('id', '==', $d_t->drink_id)->get();
+            try {
+                if ($ans->count() > 0)
+                    return $ans;
+            } catch (\Exception $e) {
+                return [];
+            }
+        } catch (\Exception $e) {
+            return [];
+        }
+        return [];
+    }
 
 }
