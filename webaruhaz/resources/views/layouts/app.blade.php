1c830e7b936eeb9317d815c6a67169f95ed11e49
Nagy Zoltan <zolya1999@gmail.com>
2021-04-16 15:13:29 +0200

    Profil felvéve a legördülő menübe

--- webaruhaz/resources/views/layouts/app.blade.php
+++ webaruhaz/resources/views/layouts/app.blade.php
@@ -54,7 +54,8 @@
                                     {{ Auth::user()->name }}
                                 </a>
                                 <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item-text" href="{{route("shop")}}">Shop</a>
+                                    <a class="dropdown-item-text" href="{{route("shop")}}">Üzlet</a>
+                                    <a class="dropdown-item-text" href="{{route("profile")}}">Profil</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
