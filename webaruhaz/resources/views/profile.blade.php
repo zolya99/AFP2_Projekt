a15e71aa0eb5ced81a87de90d31dd6b9cdd41557
Nyeste Reka <rekabeka37@gmail.com>
2021-04-22 18:10:17 +0200

    Profil oldalhoz bootstrap hozzárendelése

--- webaruhaz/resources/views/profile.blade.php
+++ webaruhaz/resources/views/profile.blade.php
@@ -1,51 +1,32 @@
 @extends('layouts.app')
-
 @section('content')
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <div class="container">
         <div class="jumbotron" style="background:#F5F5F5">
-            <div class="row">
-                <div class="col-xs-12 col-sm-6 col-md-6">
-                    <div class="col-sm-6 col-md-8">
-                        <h2> Profil adatok:
-                        </h2>
-                        <div class="table responsive">
-
-
-                            <div class="divTableBody">
-                                <div class="divTableRow">
-                                    <div class="divTableCell">Név: {{ Auth::user()->name }}</div>
-                                </div>
-                                <div class="divTableRow">
-                                    <div class="divTableCell">Email: {{ Auth::user()->email }}</div>
-                                </div>
-                                <div class="divTableRow">
-                                    <div class="divTableCell">Telefonszám: {{Auth::user()->phone_number}}</div>
-                                </div>
-                                <div class="divTableRow">
-                                    <div class="divTableCell">Nem: {{Auth::user()->sex }}</div>
-                                </div>
-                                <div class="divTableRow">
-                                    <div class="divTableCell">Születési dátum: {{ Auth::user()->birth }}</div>
-                                </div>
-
-                            </div>
-
-
-
-                            <p>
-
-                            <div class="btn">
-                                <a href="{{ route('home') }}" class="btn btn-warning">Megrendelések
-                                </a><!--This should redirect the user to his/her orders and show said orders status and list the orders-->
-                            </div>
-                            <br />
-                            <div class="btn-group">
-                                <a href="{{ route('profile_edit') }}" class="btn btn-warning"> <!--This button looks different from the others for the purpose of the users not mixing them up-->
-                                    Profil szerkesztése</a> <!--This button should enable the logged in user to edit his/her profile-->
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <h2> <i class="glyphicon glyphicon-user"></i> {{ Auth::user()->name }}</h2>
+            <div class="table responsive">
+                <img src="http://placehold.it/300x300" alt="" class="img-rounded img-responsive" />
+            </div>
+            <p>
+                <i class="glyphicon glyphicon-map-marker"></i> {{ Auth::user()->address }}
+                <br/>
+                <i class="glyphicon glyphicon-envelope"></i> {{ Auth::user()->email }}
+                <br />
+                <i class="glyphicon glyphicon-phone"></i> {{ Auth::user()->phone_number }}
+                <br />
+                <i class="glyphicon glyphicon-gift"></i> {{ Auth::user()->birth }}
+            </p>
+            <div class="btn">
+                <a href="{{ route('home') }}" class="btn btn-warning">Megrendelések
+                </a>
+            </div>
+            <br />
+            <div class="btn-group">
+                <a href="{{ route('profile_edit') }}" class="btn btn-warning">
+                    Profil szerkesztése</a>
             </div>
         </div>
+    </div>
 @endsection
