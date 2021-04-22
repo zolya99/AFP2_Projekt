ecb825acbca2376e3afde68bd7a9ff1f27b8547b
Kungl Rebeka <kungl.rebeka@gmail.com>
2021-04-22 23:12:17 +0200

    Frontend-upload.blade.php formázás
    Háttér, footer, header formázása

--- webaruhaz/resources/views/upload.blade.php
+++ webaruhaz/resources/views/upload.blade.php
@@ -2,10 +2,24 @@
 
 @section('content')
 
+    <div class ="bg"
+         style ="
+             background-image: url({{url('image/bg.jpg')}});
+             height: 100%;
+             background-position: center;
+             background-repeat: no-repeat;
+             background-size: cover;">
+
+        <div class="header" style="position: relative;text-align: center;color: white;">
+            <a class="dropdown-item-text" href="{{route("shop")}}"><img src="{{url('/image/headerpurple.jpg')}}" alt="header" style="width:100%; height: 200px"/></a>
+            <div class="header-text" style="color:black; text-align:center; font-family: Nunito; position:absolute; top:50%;left: 50%;transform: translate(-50%, -50%);">
+                <h1>Nunito Italker</h1>
+            </div>
+        </div>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
+                <div class="card"style="background-color:transparent;">
                     <div class="card-header">{{ __('Adatok frissítése') }}</div>
 
                     <div class="card-body">
@@ -44,7 +58,7 @@
 
                                     <div class="form-group row mb-0">
                                         <div class="col-md-6 offset-md-4">
-                                            <button type="submit" class="btn btn-primary">
+                                            <button type="submit" class="btn btn-primary" style="background-color: purple">
                                                 {{ __('Frissítés') }}
                                             </button>
                                         </div>
@@ -53,6 +67,10 @@
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+        <div class="footer" style="position: fixed;left: 0; bottom: 0; width: 100%;background-color: white; color: black;text-align: center;">
+            <p>Impact 2.0 ©Copyright 2021</p>
         </div>
     </div>
 @endsection
