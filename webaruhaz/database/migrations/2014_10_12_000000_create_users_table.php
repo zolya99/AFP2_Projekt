6f69ccd52da9a1c9e2de3e5abab80d026d3d2160
Nagy Zoltan <zolya1999@gmail.com>
2021-04-22 22:20:20 +0200

    Adatbázis migráció

--- webaruhaz/database/migrations/2014_10_12_000000_create_users_table.php
+++ webaruhaz/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('birth');
+            $table->string('sex');
+            $table->string('address');
+            $table->integer('phone_number');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ webaruhaz/database/migrations/2021_04_22_184807_create_drinks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDrinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drinks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('price');
+            $table->string('type');
+            $table->string('description');
+            $table->integer('onStock');
+            $table->string('placeOfOrigin');
+            $table->binary('picture');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drinks');
+    }
+}
