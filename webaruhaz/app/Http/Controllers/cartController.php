e7b9bd8ae77c8f5e29dab7e3569b67aa9d359618
Adam Ligart <ligartadam@gmail.com>
2021-04-22 23:08:22 +0200

    Backend - Cartcontroller törlés funckió elkészítése
    Backend - Cartcontroller törlés funckió elkészítése...

--- webaruhaz/app/Http/Controllers/cartController.php
+++ webaruhaz/app/Http/Controllers/cartController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Drink;
+use App\Helpers\AppHelper;
+use App\Order;
+use App\Package;
 
 class cartController extends Controller
 {
@@ -28,5 +31,12 @@ class cartController extends Controller
         }
 
     }
+    public function remove($id){
+        $this->getUserId($id);
+        $order_id = Order::getCartIDFor($id);
+        Package::DeleteWhere($order_id, $id);
+        return redirect('cart');
+    }
+
 }
 ?>
