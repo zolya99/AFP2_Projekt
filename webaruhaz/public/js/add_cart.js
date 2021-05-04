dbad7d155c24a5cefda122fe18a033d1656f1167
Adam Ligart <ligartadam@gmail.com>
2021-05-04 22:33:51 +0200

    Javascript - kosárba helyezés gomb
    Javascript - kosárba helyezés gomb megírása...

--- webaruhaz/public/js/add_cart.js
+++
@@ -1,6 +0,0 @@
-function add_to_cart(link) {
-    $.get(link, function (data, status) {
-        let count = parseInt($('#user_cart_count').text());
-        $('#user_cart_count').text(count + 1);
-    });
-}
