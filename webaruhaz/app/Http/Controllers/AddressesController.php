44b9fdc01ca41f5637bbf97e1eb315c3c451a8a2
Nagy Zoltan <zolya1999@gmail.com>
2021-05-04 22:19:59 +0200

    AddressesController létrehozva

---
+++ webaruhaz/app/Http/Controllers/AddressesController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Addresses;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AddressesController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Addresses $addresses
+     * @return Response
+     */
+    public function show(Addresses $addresses)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Addresses $addresses
+     * @return Response
+     */
+    public function edit(Addresses $addresses)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Addresses $addresses
+     * @return Response
+     */
+    public function update(Request $request, Addresses $addresses)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Addresses $addresses
+     * @return Response
+     */
+    public function destroy(Addresses $addresses)
+    {
+        //
+    }
+}
