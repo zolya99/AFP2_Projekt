44871d8d4fd7c8639327c257466f545fdf548b24
Kungl Rebeka <kungl.rebeka@gmail.com>
2021-05-04 16:49:08 +0200

    Backend - profileController.php elérési útvonal javítása
    Apphelper elérési útvonalának javítása

--- webaruhaz/app/Http/Controllers/profileController.php
+++ webaruhaz/app/Http/Controllers/profileController.php
@@ -4,7 +4,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-use App\Http\Controllers\AppHelper;
+use App\Helpers\AppHelper;
 
 
 class profileController
