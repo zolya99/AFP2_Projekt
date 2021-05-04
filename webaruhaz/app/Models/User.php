f05cc71dbda627458ab867b1170fa087c1d65c02
Kungl Rebeka <kungl.rebeka@gmail.com>
2021-05-04 16:48:28 +0200

    Backend - User.php Addresses,Apphelper,Auth,Crypt és Cookie importálása
    Elérési útvonalak javítása

--- webaruhaz/app/Models/User.php
+++ webaruhaz/app/Models/User.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\Addresses;
+use App\Helpers\AppHelper;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Crypt;
+use Symfony\Component\HttpFoundation\Cookie;
+
 
 class User extends Authenticatable
 {
@@ -44,6 +50,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    /**
+     * @var mixed
+     */
+
 
     public static function testUser() : User
     {
