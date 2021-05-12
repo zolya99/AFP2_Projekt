6c0217eb55536838019d67a2e199e958d52a6365
Nyeste Reka <rekabeka37@gmail.com>
2021-05-12 20:18:17 +0200

    profil.blade.php oldalon Profil oldalcím beállítva

--- webaruhaz/resources/views/profile.blade.php
+++ webaruhaz/resources/views/profile.blade.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <title>Profil</title>
+    <meta charset="utf-8">
+</head>
+</html>
 @extends('layouts.app')
 @section('content')
     <div class ="bg"
