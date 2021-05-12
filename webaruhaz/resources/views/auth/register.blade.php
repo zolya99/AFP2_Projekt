5f68a080b7550bf799282e1941ee8545e9816dd4
Nyeste Reka <rekabeka37@gmail.com>
2021-05-12 20:39:57 +0200

    Regisztráció oldalon Register-re javítva az oldal címe

--- webaruhaz/resources/views/auth/register.blade.php
+++ webaruhaz/resources/views/auth/register.blade.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <title>Register</title>
+    <meta charset="utf-8">
+</head>
+</html>
 @extends('layouts.app')
 
 @section('content')
