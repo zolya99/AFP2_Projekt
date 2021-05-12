e249d4af7407e34bd477675ee3f0aa0673b99b52
Adam Ligart <ligartadam@gmail.com>
2021-05-12 21:10:10 +0200

    HTML - shop.blade végső keret elkészítése.
    HTML - shop.blade végső keret elkészítése...

--- webaruhaz/resources/views/shop.blade.php
+++ webaruhaz/resources/views/shop.blade.php
@@ -48,7 +48,7 @@
                                 <div class="card-body">
                                     <form class="pb-3">
                                         <div class="input-group">
-                                            <input type="text" class="form-control" placeholder="Keresés" id="quick_search">
+                                            <input type="text" class="form-control" placeholder="Fejlesztés alatt" id="quick_search">
                                         </div>
                                     </form>
                                 </div> <!-- card-body.// -->
@@ -115,16 +115,6 @@
                             </div>
                         </article> <!-- filter-group .// -->
                         <article class="filter-group">
-                            <header class="card-header" >
-                                <a href="#" data-toggle="collapse" data-target="#collapse_4" aria-expanded="false" class="">
-                                    <i class="icon-control orange fa fa-chevron-down"></i>
-                                    <h6 class="title" style="color:black;">Ár
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shop" viewBox="0 0 16 16">
-                                            <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z"/>
-                                        </svg>
-                                    </h6>
-                                </a>
-                            </header>
                             <div class="filter-content collapse in"  id="collapse_4" style="">
                                 <div class="card-body">
                                     <div class="form-row">
@@ -141,17 +131,6 @@
                             </div>
                         </article> <!-- filter-group .// -->
                         <article class="filter-group">
-                            <header class="card-header">
-                                <a href="#" data-toggle="collapse" data-target="#collapse_6" aria-expanded="false" class="">
-                                    <i class="icon-control orange fa fa-chevron-down "></i>
-                                    <h6 class="title" style="color:black;">További szűrők
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-funnel-fill" viewBox="0 0 16 16">
-                                            <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2z"/>
-                                        </svg>
-                                    </h6>
-
-                                </a>
-                            </header>
                             <div class="filter-content collapse in" id="collapse_6" style="">
                                 <h6 class="title card-header">Külföldi italok</h6>
                                 <div class="card-body">
