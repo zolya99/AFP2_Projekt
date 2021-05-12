7924bd7d9276a94dd3d91b25e7de67f0b0610850
Nyeste Reka <rekabeka37@gmail.com>
2021-05-12 20:21:45 +0200

    Az admin felületen Admin oldalcím beállítva

--- webaruhaz/resources/views/upload.blade.php
+++ webaruhaz/resources/views/upload.blade.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <title>Admin</title>
+    <meta charset="utf-8">
+</head>
+</html>
 @extends('layouts.app')
 
 @section('content')
